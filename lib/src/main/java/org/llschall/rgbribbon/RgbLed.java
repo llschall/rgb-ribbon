374abb0c107dacee9faf0e8b28b4c07ed87bcf8f
llschall <llschall@gmx.com>
2025-11-01 21:52:38 +0100

    [API] Add comments

--- lib/src/main/java/org/llschall/rgbribbon/RgbLed.java
+++ lib/src/main/java/org/llschall/rgbribbon/RgbLed.java
@@ -2,24 +2,57 @@ package org.llschall.rgbribbon;
 
 import java.awt.Color;
 
+/**
+ * Represents an RGB LED with individual control over red, green, and blue components.
+ * Each color component can be set independently or all at once using a Color object.
+ */
 public class RgbLed {
 
+    /**
+     * Red component value (0-255)
+     */
     int red;
+    /**
+     * Green component value (0-255)
+     */
     int green;
+    /**
+     * Blue component value (0-255)
+     */
     int blue;
 
+    /**
+     * Sets the red component of the LED
+     *
+     * @param value The red intensity (0-255)
+     */
     public void setRed(int value) {
         red = value;
     }
 
+    /**
+     * Sets the green component of the LED
+     *
+     * @param value The green intensity (0-255)
+     */
     public void setGreen(int value) {
         green = value;
     }
 
+    /**
+     * Sets the blue component of the LED
+     *
+     * @param value The blue intensity (0-255)
+     */
     public void setBlue(int value) {
         blue = value;
     }
 
+    /**
+     * Sets all color components of the LED using a Color object
+     *
+     * @param color The Color object containing RGB values
+     */
     public void setColor(Color color) {
         red = color.getRed();
         green = color.getGreen();
--- lib/src/main/java/org/llschall/rgbribbon/RgbRibbon.java
+++ lib/src/main/java/org/llschall/rgbribbon/RgbRibbon.java
@@ -1,31 +1,66 @@
 package org.llschall.rgbribbon;
 
+/**
+ * Controls an RGB LED ribbon/strip, providing functionality to manage individual LEDs
+ * and overall ribbon settings.
+ */
 public class RgbRibbon {
 
+    /**
+     * Current version of the RGB Ribbon library
+     */
     public static String VERSION = RgbRibbonImpl.VERSION;
 
+    /**
+     * Implementation object that handles the low-level ribbon control
+     */
     final RgbRibbonImpl impl;
 
+    /**
+     * Creates a new RGB ribbon controller with specified brightness
+     *
+     * @param brightness Initial brightness level for the ribbon (0-255)
+     */
     public RgbRibbon(int brightness) {
         impl = new RgbRibbonImpl();
     }
 
+    /**
+     * Initializes and starts the ribbon operation
+     */
     public void start() {
         impl.start();
     }
 
+    /**
+     * Toggles the built-in LED state (on/off)
+     */
     public void toggleBuiltInLed() {
         impl.toggleBuiltInLed();
     }
 
+    /**
+     * Publishes the current LED states to the physical ribbon
+     */
     public void publish() {
         impl.publish();
     }
 
+    /**
+     * Gets a specific LED from the ribbon for individual control
+     *
+     * @param i Index of the LED to control
+     * @return RgbLed object representing the LED at the specified index
+     */
     public RgbLed getLed(int i) {
         return impl.getLed(i);
     }
 
+    /**
+     * Updates the overall brightness of the ribbon
+     *
+     * @param brightness New brightness value (0-255)
+     */
     public void setBrightness(int brightness) {
         impl.updateBrightness(brightness);
     }
