663655f87b16c89babd7af6b0906407563f617b5
llschall <llschall@gmx.com>
2026-01-20 21:59:20 +0100

    [Build] Bump to version 0.0.8

--- lib/src/main/kotlin/org/llschall/rgb/ribbon/RgbRibbonImpl.kt
+++ lib/src/main/kotlin/org/llschall/rgb/ribbon/RgbRibbonImpl.kt
@@ -10,7 +10,7 @@ class RgbRibbonImpl() {
 
     companion object {
         @JvmField
-        var VERSION: String = "0.0.7"
+        var VERSION: String = "0.0.8"
     }
 
     val program = ArdwProgram()
